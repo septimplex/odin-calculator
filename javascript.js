66716ce6d9eeab9327537ef7e650de79152b959c
Septimplex <septimplexx@gmail.com>
2023-04-07 18:25:53 +0200

    Add operate function

--- javascript.js
+++ javascript.js
@@ -18,3 +18,18 @@ const number1 = 0;
 const number2 = 0;
 const operand = "";
 
+function operate(num1,num2,operand){
+    if(operand == "+"){
+        return add(num1,num2);
+    }
+    else if(operand == "-"){
+        return subtract(num1,num2);
+    }
+    else if(operand == "*"){
+        return multiply(num1,num2);
+    }
+    else{
+        return divide(num1,num2);
+    }
+}
+
