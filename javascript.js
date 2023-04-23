a17c4cb43c5a9b0bee744cc49d4811108ecdedff
Septimplex <septimplexx@gmail.com>
2023-04-23 23:27:21 +0200

    fill display with numbers and return result

--- javascript.js
+++ javascript.js
@@ -23,25 +23,105 @@ function operate(num1,num2,operand){
 }
 
 let display = document.querySelector("#calculation");
-let numBtns = document.querySelectorAll(".num");
-let actionBtns = document.querySelectorAll(".action");
+let btns = document.querySelectorAll("button");
 let operationBtns = document.querySelectorAll(".operation");
-let equalBtn = document.querySelector(".equal");
 
 let number1 = 0;
 let number2 = 0;
-let operand = "";
+let result = 0;
 
-numBtns.forEach(btn => btn.addEventListener('click', function(){
-    display.innerText += btn.value;
-    if (display.innerText[0] === "0"){
-        display.innerText = display.innerText.substring(1);
+let current = 0;
+
+operand = "";
+
+
+btns.forEach(btn => btn.addEventListener('click', function(){
+
+    // ako je number i current je nula
+    //samo displayajaj broj
+    if(btn.className == "num"){
+        if(current == 0){
+         display.innerText += btn.value;
+                if (display.innerText[0] === "0"){
+                    display.innerText = display.innerText.substring(1);
+                }  
+            
+            }
+        else {
+            console.log(current);
+            console.log(operand);
+        }
+        }
+
+        
+       
+    
+   
+
+    if(btn.className == "operation"){
+        operand = btn.value;
+        current = display.innerText;
+        
+         
+    }
+
+    
+
+    
+
+    
+    
+        
+
+    
+    
+    
+
+    
+    /*
+    if(number1 > 0 && number2 > 0 && operand != ""){
+        let result = operate(number1, number2, operand);
+        display.innerText = result;
+        number1 = result;
+    }
+
+    if(btn.className == "num"){
+    if(number1 == 0){
+        display.innerText += btn.value;
+            if (display.innerText[0] === "0"){
+                display.innerText = display.innerText.substring(1);
+            }
+            
+        number1 = display.innerText;
+    } 
+
+
+    else{
+        display.innerText = "";
+
     }
 }
-));
 
-operationBtns.forEach(btn => btn.addEventListener('click', function(){
-    console.log(btn.innerText);
+    
+
+    
+
+    if(btn.className == "num"){
+        display.innerText += btn.value;
+        if (display.innerText[0] === "0"){
+            display.innerText = display.innerText.substring(1);
+        }
+
+    }
+
+    if(btn.className == "operation"){
+        if(operand == ""){
+            number1 = display.innerText;
+        }
+    }*/
+    
 }
 ));
+
+
 
